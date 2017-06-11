a39f8f66c78b535c4a59e407d301ba951324c4d4
Simon Brungs <00pflaume@gmail.com>
2017-06-11 11:05:51 +0200

    First commit

---
+++ src/main/java/de/simonsator/partyandfriends/colorperms/ColorPack.java
@@ -0,0 +1,15 @@
+package de.simonsator.partyandfriends.colorperms;
+
+/**
+ * @author 00pfl
+ * @version 1.0 09.06.2017.
+ */
+public class ColorPack {
+    public final String COLOR;
+    public final String PERMISSION;
+
+    public ColorPack(String pColor, String pPermission) {
+        COLOR = pColor;
+        PERMISSION = pPermission;
+    }
+}
---
+++ src/main/java/de/simonsator/partyandfriends/colorperms/ColorPermsPlugin.java
@@ -0,0 +1,64 @@
+package de.simonsator.partyandfriends.colorperms;
+
+import de.simonsator.partyandfriends.api.PAFExtension;
+import de.simonsator.partyandfriends.api.pafplayers.DisplayNameProvider;
+import de.simonsator.partyandfriends.api.pafplayers.OnlinePAFPlayer;
+import de.simonsator.partyandfriends.api.pafplayers.PAFPlayer;
+import de.simonsator.partyandfriends.api.pafplayers.PAFPlayerClass;
+import de.simonsator.partyandfriends.colorperms.configuration.ColorPermsConfig;
+import net.md_5.bungee.config.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Simonsator
+ * @version 1.0 09.06.2017.
+ */
+public class ColorPermsPlugin extends PAFExtension implements DisplayNameProvider {
+    private String defaultColor;
+    private List<ColorPack> colors;
+    private String offlineColor;
+
+    @Override
+    public void onEnable() {
+        try {
+            Configuration config = new ColorPermsConfig(new File(getConfigFolder(), "config.yml")).getCreatedConfiguration();
+            defaultColor = config.getString("General.DefaultColor");
+            offlineColor = config.getString("General.OfflineColor");
+            colors = new LinkedList<>();
+            for (String key : config.getSection("Colors").getKeys()) {
+                colors.add(new ColorPack(config.getString("Colors." + key + ".Color"), config.getString("Colors." + key + ".Permission")));
+                System.out.println(key + config.getString("Colors." + key + ".Color") + config.getString("Colors." + key + ".Permission"));
+            }
+            PAFPlayerClass.setDisplayNameProvider(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void reload() {
+        onDisable();
+        onEnable();
+    }
+
+    @Override
+    public String getDisplayName(PAFPlayer pPlayer) {
+        return offlineColor + pPlayer.getName();
+    }
+
+    @Override
+    public String getDisplayName(OnlinePAFPlayer pPlayer) {
+        for (ColorPack colorPack : colors) {
+            if (pPlayer.hasPermission(colorPack.PERMISSION)) {
+                System.out.println(colorPack.COLOR + pPlayer.getName());
+                return colorPack.COLOR + pPlayer.getName();
+            }
+        }
+        System.out.println(defaultColor + pPlayer.getName());
+        return defaultColor + pPlayer.getName();
+    }
+}
